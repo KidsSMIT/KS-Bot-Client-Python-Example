02e415bcec0190a82846253e5d5c83a5e51bee6d
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2022-02-15 15:18:14 +0000

    Updated to reflect new versions of KS-BOT-Client-Python

--- main.py
+++ main.py
@@ -38,6 +38,36 @@ class CustomBot(ks_bot_client.Bot):
 
     print("KS-BOT said: Timer is over, Timer: ", data)
 
+  def weather(self, data):
+    """
+      This event runs whenever the server has weather report ready for you
+    """
+    pass
+
+  def news(self, data):
+    """
+      This event runs whenever server has the news ready for you
+    """
+    pass
+
+  def speak(self, what_to_speak):
+    """
+      This events runs when ever the server wants you to say something
+    """
+    pass
+
+  def de_activate_smith(self, data):
+    """
+      This events runs after S.M.I.T.H has been De-Activated and KS-BOT has been activated
+    """
+    pass
+
+  def activate_smith(self, data):
+    """
+      This event runs after S.M.I.T.H has been activated
+    """
+    pass
+
 # To create a user account go to https://www.kidssmit.com
 bot = CustomBot("<your name>", "<your password>")
 bot.run()
